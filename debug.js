c0a50b58f3090cfc974311abd075032a51d4d7b8
Jakeoid <163038@cl-19.ccw.local>
2016-05-19 14:38:45 +1000

    19th May V2

--- debug.js
+++ debug.js
@@ -5,7 +5,7 @@
  }
 
  function updateValue() {
- 	if(document.getElementById('width').innerHTML && document.getElementById('height').innerHTML){
+ 	if(document.getElementById('width').innerHTML){
     	document.getElementById('width').innerHTML = window.innerWidth;
     	document.getElementById('height').innerHTML = window.innerHeight;
     }
