765d0f77f3cd308d64cff04bb3cd1f4c0794d8fe
Jakeoid <163038@cl-19.gateway>
2016-05-19 17:02:25 +1000

    Thu 20th May V8

--- debug.js
+++ debug.js
@@ -1,12 +1,12 @@
- setupPage();
-
- function setupPage() {
-     setInterval(updateValue, 1);
- }
-
- function updateValue() {
- 	if(document.getElementById('width') !== null){
-    	document.getElementById('width').innerHTML = window.innerWidth;
-    	document.getElementById('height').innerHTML = window.innerHeight;
-    }
- }
+// setupPage();
+//
+// function setupPage() {
+//     setInterval(updateValue, 1);
+// }
+//
+// function updateValue() {
+// 	if(document.getElementById('width') !== null){
+//    	document.getElementById('width').innerHTML = window.innerWidth;
+//    	document.getElementById('height').innerHTML = window.innerHeight;
+//    }
+// }
